dc1836484b5b06fe74108cc16c2af5e352383080
Fmstrat <nospam@nowsci.com>
2020-11-17 20:58:34 -0500

    Embed DLLs into EXE - Draft

--- LS4W/Program.cs
+++ LS4W/Program.cs
@@ -8,14 +8,22 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text.Json;
+using System.Globalization;
 using TsudaKageyu;
 
 namespace LS4W
 {
     class Program 
     {
-        static void Main(string[] args) 
+        [STAThreadAttribute]
+        public static void Main(string[] args) 
         {
+
+            AppDomain.CurrentDomain.AssemblyResolve += OnResolveAssembly;
+            //App.Main();
+            //global::App.Main();
+            //LS4W.App.Main();
+
             List<WindowsProgram> installedPrograms = new List<WindowsProgram>(); 
             string registryKey_AppPaths = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths";
 
@@ -94,5 +102,24 @@ namespace LS4W
             string jsonString = JsonSerializer.Serialize(installedPrograms);
             Console.WriteLine(jsonString);
         }
+
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            AssemblyName assemblyName = new AssemblyName(args.Name);
+
+            var path = assemblyName.Name + ".dll";
+            if (assemblyName.CultureInfo.Equals(CultureInfo.InvariantCulture) == false) path = String.Format(@"{0}\{1}", assemblyName.CultureInfo, path);
+
+            using (Stream stream = executingAssembly.GetManifestResourceStream(path))
+            {
+                if (stream == null) return null;
+
+                var assemblyRawBytes = new byte[stream.Length];
+                stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+                return Assembly.Load(assemblyRawBytes);
+            }
+        }
+
     }
 }
